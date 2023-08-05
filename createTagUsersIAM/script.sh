7caeca3085006a67d01e69fa4d0a8c9335f42f54
Marcus Vinícius Braga Alcântara <vinicius.redes2011@gmail.com>
2023-08-05 02:39:35 -0300

    Update script.sh

--- createTagUsersIAM/script.sh
+++ createTagUsersIAM/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-AWS_PROFILE="matinc";
+AWS_PROFILE="default";
 TAG_NAME="SERVICE_ACCOUNT";
 TAG_VALUE="false";
-USER_LIST_NAME="list-user-akamai-artur.txt";
+USER_LIST_NAME="list-users-VINICIUS.txt";
 
 function create_tag_user(){
 for USER in $(cat "$USER_LIST_NAME");
